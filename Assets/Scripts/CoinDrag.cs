4b616da033649d1026cd2ef2efe2229cacc9a3b1
Yamete <boondai45@gmail.com>
2024-12-11 14:45:51 +0700

    Fix Movecoin add Randomcoin+timer

--- Assets/Scripts/CoinDrag.cs
+++ Assets/Scripts/CoinDrag.cs
@@ -1,26 +1,91 @@
 using UnityEngine;
-using System.Collections.Generic;
+using System.Collections;
 using CoinGame;
 
 public class CoinDrag : MonoBehaviour
 {
-    public UIManager uiManager;
     public int coinValue;
     public CoinManager manager;
-    
-    private Dictionary<int, int> sortedCoins = new Dictionary<int, int>()
-    {
-        {1, 0},
-        {5, 0},
-        {10, 0}
-    };
+    private bool isDragging = false;
+    private Vector3 offset;
 
-    public void AddSortedCoin(int coinValue)
+    void Start()
     {
-        sortedCoins[coinValue]++;
-        if (uiManager != null)
+        if (manager == null)
         {
-            uiManager.UpdateCoinCount(coinValue, sortedCoins[coinValue]);
+            manager = GameObject.FindObjectOfType<CoinManager>();
+            if (manager == null)
+            {
+                Debug.LogError("CoinManager not found in scene!");
+            }
         }
+    }
+
+    void OnMouseDown()
+    {
+        isDragging = true;
+        offset = transform.position - GetMouseWorldPosition();
+    }
+
+    void OnMouseDrag()
+    {
+        if (isDragging)
+        {
+            Vector3 newPosition = GetMouseWorldPosition() + offset;
+            transform.position = new Vector3(newPosition.x, newPosition.y, 0);
+        }
+    }
+
+    void OnMouseUp()
+    {
+        if (isDragging)
+        {
+            isDragging = false;
+            if (manager != null)
+            {
+                CheckCoinPlacement();
+            }
+            else
+            {
+                Debug.LogError("CoinManager not found!");
+            }
+        }
+    }
+
+    void CheckCoinPlacement()
+    {
+        if (manager == null || manager.coinTypes == null) return;
+
+        foreach (var coinType in manager.coinTypes)
+        {
+            if (coinType == null || coinType.sortingArea == null) continue;
+
+            if (coinValue == coinType.value)
+            {
+                float distance = Vector2.Distance(transform.position, coinType.sortingArea.position);
+                if (distance < 1.5f)
+                {
+                    manager.AddCoinToScore(coinValue);
+                    Destroy(gameObject);
+                    return;
+                }
+            }
+            else
+            {
+                float wrongDistance = Vector2.Distance(transform.position, coinType.sortingArea.position);
+                if (wrongDistance < 1.5f)
+                {
+                    manager.SubtractScore(coinType.value);
+                    return;
+                }
+            }
+        }
+    }
+
+    Vector3 GetMouseWorldPosition()
+    {
+        Vector3 mousePoint = Input.mousePosition;
+        mousePoint.z = -Camera.main.transform.position.z;
+        return Camera.main.ScreenToWorldPoint(mousePoint);
     }
 } 
