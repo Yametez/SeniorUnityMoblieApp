21f3b65a8c5b1eaa1e0628709a77e7fb66539f26
Yamete <boondai45@gmail.com>
2025-01-20 14:18:05 +0700

    Alert text when fail Login

---
+++ Assets/Scripts/AlertManager.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class AlertManager : MonoBehaviour
+{
+    [SerializeField] private GameObject alertPanel;
+    [SerializeField] private Text alertText;
+    private static AlertManager instance;
+
+    void Awake()
+    {
+        instance = this;
+        if (alertPanel != null)
+            alertPanel.SetActive(false);
+    }
+
+    public static void ShowAlert(string message)
+    {
+        if (instance != null)
+        {
+            instance.alertText.text = message;
+            instance.alertPanel.SetActive(true);
+            instance.StartCoroutine(instance.HideAlertAfterDelay());
+        }
+    }
+
+    private IEnumerator HideAlertAfterDelay()
+    {
+        yield return new WaitForSeconds(2f); // แสดง 2 วินาที
+        alertPanel.SetActive(false);
+    }
+
+    public void HideAlert()
+    {
+        alertPanel.SetActive(false);
+    }
+} 
