0ac716c6a55274ea6358436950475c5ac4eb7726
Yamete <boondai45@gmail.com>
2025-03-14 11:47:39 +0700

    ลองแก้app.py โดยปิดportที่ใช้

--- test-api/app.py
+++ test-api/app.py
@@ -34,5 +34,6 @@ def test():
 
 if __name__ == '__main__':
     # เปลี่ยนจาก port=3000 เป็น port=10000
-    port = int(os.environ.get('PORT', 10000))
-    app.run(host='0.0.0.0', port=3000)
+    # port = int(os.environ.get('PORT', 10000))
+    # app.run(host='0.0.0.0', port=3000)
+    app.run()
