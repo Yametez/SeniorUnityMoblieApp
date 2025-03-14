87c5c84c2915254c857d1162656ca84f4ea5ff91
Yamete <boondai45@gmail.com>
2025-03-14 13:45:40 +0700

    เตรียมไฟล์Deploy เว็บใหม่ vercel

--- test-api/config.py
+++ test-api/config.py
@@ -7,9 +7,12 @@ load_dotenv()
 
 def get_db_connection():
     connection = mysql.connector.connect(
-        host=os.getenv('DB_HOST'),
+        host='gateway01.ap-northeast-1.prod.aws.tidbcloud.com',  # TiDB host
+        port=4000,  # TiDB port
         user=os.getenv('DB_USER'),
         password=os.getenv('DB_PASSWORD'),
-        database=os.getenv('DB_DATABASE')
+        database=os.getenv('DB_DATABASE'),
+        ssl_mode='VERIFY_IDENTITY',
+        ssl_ca='/etc/ssl/certs/ca-certificates.crt'  # สำหรับ SSL connection
     )
     return connection 
